6edc28a40ad132fe543e5d31bc37106d3055a9f2
John Spentzas <gspentzas1991@gmail.com>
2020-03-20 03:02:08 +0200

    Refactored the Unit movement code

--- Assets/Code/Scripts/Unit.cs
+++ Assets/Code/Scripts/Unit.cs
@@ -29,73 +29,44 @@ public class Unit : MonoBehaviour
         IsMoving = true;
         foreach (var tile in tilePath)
         {
+            SetMovementAnimation(tile.transform.position);
             do
             {
-                MoveTowardsTile(tile);
-                yield return new WaitForEndOfFrame();
+                transform.position = Vector3.MoveTowards(transform.position, tile.transform.position, Speed * Time.deltaTime);
+                if (transform.position!=tile.transform.position)
+                {
+                    yield return new WaitForEndOfFrame();
+                }
             } while (transform.position != tile.transform.position);
-            //} while (Mathf.Abs(transform.position.x - tile.transform.position.x)>=Speed*Time.deltaTime && 
-            //Mathf.Abs(transform.position.y - tile.transform.position.y)>=Speed*Time.deltaTime);
         }
-        //corrects the position to match a tile
-        transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0);
         IsMoving = false;
        
     }
 
     /// <summary>
-    /// Moves the unity towards the selectedTile. If the unit is close enough to the tile, it snaps on it
+    /// Sets the animator's values depending on the unit's direction
     /// </summary>
-    /// <param name="selectedTile"></param>
-    private void MoveTowardsTile(Tile selectedTile)
+    private void SetMovementAnimation(Vector3 movementTarget)
     {
-        float newPositionX = transform.position.x;
-        float newPositionY = transform.position.y;
-        //checks if we should move on the X or Y axis
-        if (transform.position.x == selectedTile.transform.position.x)
+        if (transform.position.x > movementTarget.x)
         {
-            if (transform.position.y < selectedTile.transform.position.y)
-            {
-                direction = MovementDirection.Up;
-                newPositionY = transform.position.y + Speed * Time.deltaTime;
-                if (newPositionY > selectedTile.transform.position.y)
-                {
-                    newPositionY = selectedTile.transform.position.y;
-                }
-            }
-            else if(transform.position.y > selectedTile.transform.position.y)
-            {
-                direction = MovementDirection.Down;
-                newPositionY = transform.position.y - Speed * Time.deltaTime;
-                if (newPositionY < selectedTile.transform.position.y)
-                {
-                    newPositionY = selectedTile.transform.position.y;
-                }
-            }
+            direction = MovementDirection.Left;
         }
-        else
+        else if (transform.position.x < movementTarget.x)
         {
-            if (transform.position.x < selectedTile.transform.position.x)
-            {
-                direction = MovementDirection.Right;
-                newPositionX = transform.position.x + Speed * Time.deltaTime;
-                if (newPositionX > selectedTile.transform.position.x)
-                {
-                    newPositionX = selectedTile.transform.position.x;
-                }
-            }
-            else if (transform.position.x > selectedTile.transform.position.x)
-            {
-                direction = MovementDirection.Left;
-                newPositionX = transform.position.x - Speed * Time.deltaTime;
-                if (newPositionX < selectedTile.transform.position.x)
-                {
-                    newPositionX = selectedTile.transform.position.x;
-                }
-            }
+
+            direction = MovementDirection.Right;
+        }
+        else if (transform.position.y > movementTarget.y)
+        {
+
+            direction = MovementDirection.Down;
+        }
+        else if (transform.position.y < movementTarget.y)
+        {
+
+            direction = MovementDirection.Up;
         }
         animator.SetInteger("Direction", (int)direction);
-        transform.position = new Vector3(newPositionX, newPositionY, transform.position.z);
-
     }
 }
